df4a91ec1897bf9ff86fe19709e92279b8384339
Xr7-Code <alexander-ludwig@tutamail.com>
2026-04-18 11:23:09 +0200

    Delete configs/rofi/powermenu.sh

--- configs/rofi/powermenu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-chosen=$(echo -e "Beenden\nNeustart\nStandby\nSperren" | rofi -dmenu -i -p "SnowFox Power:" -theme ~/.config/rofi/config.rasi)
-
-case "$chosen" in
-    "Beenden") poweroff ;;
-    "Neustart") reboot ;;
-    "Standby") systemctl suspend ;;
-    "Sperren") i3lock -c 0f0f0f ;;
-esac
